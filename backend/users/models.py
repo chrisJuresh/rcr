30a9c70541da2a0ed37ab5c3747923c68ab5595b
chrisJuresh <Christian159260@gmail.com>
2024-02-28 15:31:58 +0000

    fixed superuser

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class Role(models.Model):
     """
@@ -21,11 +21,42 @@ class Role(models.Model):
     def __str__(self):
         return self.get_name_display()
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 class User(AbstractUser):
     """
     Custom user model that extends AbstractUser with roles and titles.
     """
+    username = None
     email = models.EmailField(unique=True, verbose_name='Email Address')
+
+    objects = UserManager()  
+    
     USERNAME_FIELD = 'email'  
     REQUIRED_FIELDS = []  
     
@@ -37,6 +68,7 @@ class User(AbstractUser):
         verbose_name='User Roles'
     )
     title = models.CharField(
+        null=True,
         max_length=4,
         blank=True,
         verbose_name='Title'
