04e40595d9c468e8876c0f4c4d9251eec3f5336e
chrisJuresh <Christian159260@gmail.com>
2024-03-16 17:34:50 +0000

    code cleanup

--- frontend/src/routes/auth/+page.server.ts
+++ frontend/src/routes/auth/+page.server.ts
@@ -8,26 +8,41 @@ import axios from 'axios';
 import postmark from 'postmark';
 
 export const load: PageServerLoad = async (event) => {
-	let loggedIn = false;
-	if (event.cookies.get('token')) {
-		try {
-			await axios.post('http://localhost:8000/api/token/verify', {
-				token: event.cookies.get('token')
-			});
-			loggedIn = true;
-		} catch (error) {
-			// stay on /auth
-		} finally {
-			if (loggedIn) {
+	const token = event.cookies.get('token');
+	if (token) {
+		await axios
+			.post('http://localhost:8000/api/token/verify', {
+				token: token
+			})
+			.then(() => {
 				redirect(302, '/protected/profile');
-			}
-		}
+			});
 	}
 
 	return {
 		loginForm: await superValidate(zod(loginFormSchema)),
 		registerForm: await superValidate(zod(registerFormSchema))
 	};
+};
+
+const sendVerificationEmail = async (email, token) => {
+	const verificationUrl = `http://localhost:5173/auth/verify?token=${token}`;
+	console.log(verificationUrl);
+	//const client = new postmark.ServerClient('cd8d27e7-e383-4d6d-ad5e-daa45fbcd2f5');
+	//
+	//try {
+	//	await client.sendEmail({
+	//		From: 'verify@chrisj.uk',
+	//		To: email,
+	//		Subject: 'Verify your email',
+	//		HtmlBody: `<strong>Please verify your email</strong> by clicking <a href="${verificationUrl}">here</a>.`,
+	//		TextBody: `Please verify your email by visiting this link: ${verificationUrl}`,
+	//		MessageStream: 'outbound'
+	//	});
+	//} catch (error) {
+	//	console.error('Failed to send verification email:', error);
+	//	throw error;
+	//}
 };
 
 const setTokenCookie = (event, value) => {
@@ -40,72 +55,45 @@ const setTokenCookie = (event, value) => {
 	});
 };
 
-const handleUserForm = async (event, schema, url) => {
-	const form = await superValidate(event, zod(schema));
-	if (!form.valid) {
-		return fail(400, { form });
-	}
-	const response = await axios
-		.post(url, {
-			validateStatus: () => true,
-			email: form.data.email,
-			password: form.data.password
-		})
-		.catch((error) => {
-			return error.response;
-		});
-	setTokenCookie(event, response.data.access);
-	if (response.status === 200) {
-		redirect(302, '/protected/profile');
-	} else {
-		console.log(response.data.detail);
-		return error(response.status, { message: response.data.detail });
-	}
-};
-
-const sendVerificationEmail = async (email, token) => {
-  const verificationUrl = `http://localhost:5173/auth/verify?token=${token}`;
-//const client = new postmark.ServerClient("cd8d27e7-e383-4d6d-ad5e-daa45fbcd2f5");
-
-//try {
-//await client.sendEmail({
-//"From": "verify@chrisj.uk",
-//"To": email,
-//"Subject": "Verify your email",
-//"HtmlBody": `<strong>Please verify your email</strong> by clicking <a href="${verificationUrl}">here</a>.`,
-//"TextBody": `Please verify your email by visiting this link: ${verificationUrl}`,
-//"MessageStream": "outbound"  
-//});
-//} catch (error) {
-//console.error("Failed to send verification email:", error);
-//throw error; 
-//}
-	};
-
 export const actions: Actions = {
 	login: async (event) => {
-		await handleUserForm(event, loginFormSchema, 'http://localhost:8000/api/token/pair');
+		const form = await superValidate(event, zod(loginFormSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
+		await axios
+			.post('http://localhost:8000/api/token/pair', {
+				email: form.data.email,
+				password: form.data.password
+			})
+			.then((response) => {
+				setTokenCookie(event, response.data.access);
+				redirect(302, '/protected/profile');
+			})
+			.catch((err) => {
+				error(400, { message: err.response.data.detail });
+			});
 	},
 
 	register: async (event) => {
+		const form = await superValidate(event, zod(registerFormSchema));
+		if (!form.valid) {
+			return fail(400, { form });
+		}
 
-	const form = await superValidate(event, zod(registerFormSchema));
-	if (!form.valid) {
-		return fail(400, { form });
-	}
- 
-	const verificationToken = uuidv4();
-	await axios
-		.post( 'http://localhost:8000/api/users/register-unauthenticated', {
-			validateStatus: () => true,
-			email: form.data.email,
-			password: form.data.password,
-			token: verificationToken
-		}).catch((issue) => {
-			console.log(issue)
-		error(400, {message: issue.response.data})
-	});
-	
-	sendVerificationEmail(form.data.email, verificationToken);
+		const verificationToken = uuidv4();
+		await axios
+			.post('http://localhost:8000/api/users/register-unauthenticated', {
+				email: form.data.email,
+				password: form.data.password,
+				token: verificationToken
+			})
+			.then(() => {
+				sendVerificationEmail(form.data.email, verificationToken);
+			})
+			.catch((err) => {
+				error(400, { message: err.response.data });
+			});
 	}
 };
